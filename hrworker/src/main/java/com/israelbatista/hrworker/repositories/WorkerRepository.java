2761edaa092574ec375066fbce686268fc2f3c31
Israel Batista <israelbatiista19@gmail.com>
2021-08-08 14:54:18 -0300

    hr-worker implementation

---
+++ hrworker/src/main/java/com/israelbatista/hrworker/repositories/WorkerRepository.java
@@ -0,0 +1,7 @@
+package com.israelbatista.hrworker.repositories;
+
+import com.israelbatista.hrworker.entities.Worker;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WorkerRepository extends JpaRepository<Worker, Long> {
+}
